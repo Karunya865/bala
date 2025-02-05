b4f5e36e5dc21c3b5a8b283a1448068d84f5ee24
Karunya865 <135206135+Karunya865@users.noreply.github.com>
2025-02-05 11:49:46 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     
     sh './build.sh'
-    docker login -u karunya08 -p karunya
+    docker login -u karunya08 -p karunya@2005
     docker tag test karunya08/pushimages
     docker push karunya08/pushimages
     
